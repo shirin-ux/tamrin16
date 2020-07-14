e9945c22a83969789e453f186fec937ceb0be732
shirin-ux <64709896+shirin-ux@users.noreply.github.com>
2020-07-14 17:18:28 +0430

    Create cricle.py

---
+++ cricle.py
@@ -0,0 +1,8 @@
+recponse = input("what is your radius?")
+r = float(recponse)
+d=2*r
+print("the d is".Diameter)
+area = 3.14159*r**2
+print("the area is".area)
+environment=3.14159*d
+print("the enviroment is".inviroment)
